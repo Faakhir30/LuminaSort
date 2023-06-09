763dc35ad6129573d73f87586f8e3092e29146a1
Faakhir30 <fzahid.bscs22seecs@seecs.edu.pk>
2023-06-10 01:11:51 +0500

    added bubble sort

---
+++ src/Components/BubbleSort.js
@@ -0,0 +1,18 @@
+function BubbleSort(arr) {
+  const n = arr.length;
+
+  for (let i = 0; i < n - 1; i++) {
+    for (let j = 0; j < n - i - 1; j++) {
+      // Compare adjacent elements
+      if (arr[j] > arr[j + 1]) {
+        // Swap them if they are in the wrong order
+        const temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+
+  return arr;
+}
+export default BubbleSort;
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,90 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import "./styles/Header.css";
+import "./styles/Bars.css";
 const Header = () => {
-  return (
-    <div className='header'>
-      
-    </div>
-  )
-}
+  const [bars, setBars] = useState(generateRandomArray(50, 1, 100));
 
-export default Header
+  function BubbleSort() {
+    let arr = bars;
+    const n = arr.length;
+
+    let i = 0;
+    let j = 0;
+
+    const intervalId = setInterval(() => {
+      if (i < n - 1) {
+        if (j < n - i - 1) {
+          // Compare adjacent elements
+          if (arr[j] > arr[j + 1]) {
+            // Swap them if they are in the wrong order
+            const temp = arr[j];
+            arr[j] = arr[j + 1];
+            arr[j + 1] = temp;
+            setBars([...arr]);
+          }
+          j++;
+        } else {
+          // Move to the next iteration of i and reset j
+          i++;
+          j = 0;
+        }
+      } else {
+        // Sorting is complete, cancel the interval
+        clearInterval(intervalId);
+      }
+    }, 50);
+  }
+  function handleBars(bars) {
+    const oldBars = document.getElementsByClassName("bars");
+    oldBars.innerHTML = plotBars(bars);
+  }
+
+  function setRandomBars(e) {
+    e.preventDefault();
+    setBars(generateRandomArray(50, 1, 100));
+  }
+
+  function generateRandomArray(length, min, max) {
+    const randomArray = [];
+
+    for (let i = 0; i < length; i++) {
+      const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+      randomArray.push(randomNumber);
+    }
+
+    return randomArray;
+  }
+
+  return (
+    <>
+      <div className="header">
+        <button>logo</button>
+        <div>
+          <button onClick={setRandomBars}>Generate random Array</button>
+          <button>speed</button>
+          <button onClick={BubbleSort}>Bubble sort</button>
+          <button>start</button>
+        </div>
+      </div>
+      <div className="bars">{plotBars(bars)}</div>
+    </>
+  );
+};
+
+export default Header;
+
+function plotBars(bars) {
+  return bars.map((element, index) => (
+    <div
+      key={index}
+      className="bar"
+      style={{
+        height: `${(element / Math.max(...bars)) * 70}vh`,
+        width: `${40 / bars.length}vw`,
+        marginRight: `${4 / bars.length}vw`,
+      }}
+    >
+      {element}
+    </div>
+  ));
+}

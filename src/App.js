844bc858cf85cc693688475de65ea5dccb7ac308
Faakhir30 <fzahid.bscs22seecs@seecs.edu.pk>
2023-06-11 04:30:18 +0500

    added loading screen

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.css';
-import Header from './Components/Header';
+import React, { useState, useEffect } from 'react';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+import "./Components/styles/Header.css";
+
+import Header from "./Components/Header";
+import Loader from "./Components/Loader"; // Import the Loader component
+
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate loading data or asynchronous operation
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+  }, []);
+
   return (
-    <div className="App">
-      <Header/>
-    </div>
+    <>
+      <div className="App">
+        {isLoading ? (
+          <Loader /> // Display the Loader component when isLoading is true
+        ) : (
+          <Header /> // Render the Header component when isLoading is false
+        )}
+      </div>
+    </>
   );
 }
 
